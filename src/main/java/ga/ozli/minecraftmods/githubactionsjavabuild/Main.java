771239c10fd0adcc35b3bed7699c8b36033d22b9
Paint_Ninja <PaintNinja@users.noreply.github.com>
2023-09-01 20:25:22 +0100

    Update Main.java

--- src/main/java/ga/ozli/minecraftmods/githubactionsjavabuild/Main.java
+++ src/main/java/ga/ozli/minecraftmods/githubactionsjavabuild/Main.java
@@ -1,7 +1,7 @@
 package ga.ozli.minecraftmods.githubactionsjavabuild;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println("Hello, world!");
     }
 }
